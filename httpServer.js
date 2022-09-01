7560d00f4f58f5bd4d42c3d955b676d1a33aeb5e
Hyonony <80375871+Hyonony@users.noreply.github.com>
2022-09-01 11:43:28 +0900

    Update httpServer.js

--- httpServer.js
+++ httpServer.js
@@ -1,13 +1,16 @@
 
 const net = require('net')
+const serveStatic = require('serve-static')
       express = require('express') // 웹 프레임 워크
       app = express()
       httpserver = require('http').createServer(app), //httpserver
       io = require('socket.io')(httpserver), //socket.io
       require('dotenv').config(), // dotenv 환경 변수 관리하기 (.env파일)
-      http_PORT = process.env.http_PORT; // .env
-      tcp_port = process.env.tcp_port;  // .env
-      client = net.connect({port :tcp_port, host : 'localhost' });
+      path = require('path')
+      bodyParser = require('body-parser')
+      http_PORT = process.env.http_PORT // .env
+      tcp_port = process.env.tcp_port  // .env
+      client = net.connect({port :tcp_port, host : 'localhost' })
       //TCP <-> Http 연결에 필요한 객체
 
 // httpserver를 Linsten 상태로 둠
@@ -17,10 +20,11 @@ httpserver.listen(http_PORT, ()=> {
 
 // Express 사용하기 위함. 나중에는 결국 앱에서 프론트 역할을 하기에 사용하지 않을 것.
 // 가상의 페이지로 테스트 용으로 사용하는 것.
-app.use(express.static(__dirname + '/static'));
-app.get('/', (req, res) =>{
-  res.sendFile(__dirname + '/index.html');
-})
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(bodyParser.json())
+app.use('/public', serveStatic(path.join(__dirname, 'view')))
 
 //socket.io 연결됐을 경우 아래 내용이 실행됨
 io.on('connection', (socket)=> {
