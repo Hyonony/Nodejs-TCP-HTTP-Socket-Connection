9044b363ed5a46a9c0abe7e48eedeb708055e1ab
Hyonony <80375871+Hyonony@users.noreply.github.com>
2022-11-03 17:25:55 +0900

    Update httpServer.js

--- httpServer.js
+++ httpServer.js
@@ -1,54 +1,64 @@
-
-const { fstat } = require('fs')
-const net = require('net')
+const express = require('express')
+const app = express()
+const httpserver = require('http').createServer(app)
+const io = require('socket.io')(httpserver)
+const bodyParser = require('body-parser')
 const serveStatic = require('serve-static')
-      express = require('express') // 웹 프레임 워크
-      app = express()
-      httpserver = require('http').createServer(app), //httpserver
-      io = require('socket.io')(httpserver), //socket.io
-      require('dotenv').config(), // dotenv 환경 변수 관리하기 (.env파일)
-      path = require('path')
-      bodyParser = require('body-parser')
-      http_PORT = process.env.http_PORT // .env
-      tcp_port = process.env.tcp_port  // .env
-      client = net.connect({port :tcp_port, host : 'localhost' })
-      //TCP <-> Http 연결에 필요한 객체
-      mysql = require('mysql')
-      fs = require('fs');
+const path = require('path')
+require("dotenv").config();
+const net = require('net'),
+      tcpPort = process.env.tcp_port,
+      httpPort = process.env.http_port
+const client = net.connect({port: tcpPort, host:'localhost'})
+const cors = require('cors')
+
+const options = {
+  origin: 'http://localhost:3000',
+  Credential: true,
+};
+
+var count = 0;
 
 
+httpserver.listen(httpPort, ()=> {
+  console.log(`HTTP Server is listening on port ${httpPort}`)
+});
 
 
-
-// httpserver를 Linsten 상태로 둠
-httpserver.listen(http_PORT, ()=> {
-    console.log(`HTTP Server is listening on port ${http_PORT}`)
-  });
-
-// Express 사용하기 위함. 나중에는 결국 앱에서 프론트 역할을 하기에 사용하지 않을 것.
-// 가상의 페이지로 테스트 용으로 사용하는 것.
-
+app.use(cors(options))
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(__dirname + '/html'))
 
 app.use(bodyParser.json())
-app.use('/public', serveStatic(path.join(__dirname, 'view')))
+app.use('/', serveStatic(path.join(__dirname, 'view')))
 
 app.use((req, res) => {
   res.status(404).send('Not Found')
 })
 
-//socket.io 연결됐을 경우 아래 내용이 실행됨
+
+
 io.on('connection', (socket)=> {
   //연결되면 socket 파라미터에 socket 데이터가 쌓임
-
+  
+  count++
+  // const ip = socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress.split(":")[3];
+  // console.log(ip);
+  console.log('Http connection :', socket.request.connection._peername);
   // console.log(socket);
   // 확인하려면 위에 터미널에 찍어볼 수 있음
-
+  
   socket.on('message', (message)=>{
     // message라는 약속으로 통신을 하는 것 프론트와 같은 네임을 가져야함
     
     client.write(`"${message}"`)
     //TCP <-> Http 연결에 필요한 객체를 사용하여 TcpBroadCast 함수로 데이터 전송
+    io.emit('message', `${count}번 익명자 : ${message}`);
   })
+
+  socket.on('disconnect', () => {
+    console.log('Http User Disconnected');
+    count--;
+  });
 });
+
